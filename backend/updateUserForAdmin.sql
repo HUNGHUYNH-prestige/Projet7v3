e1de92a79720620c381ecb0667beaad9965ad169
Hung <hunghuynh22@gmail.com>
2021-10-01 19:09:12 +0200

    SQL uppercase

--- backend/updateUserForAdmin.sql
+++ backend/updateUserForAdmin.sql
@@ -3,4 +3,4 @@
 -- by default isAdmin is set to 0 for all users
 -- if needed, use this code to update a profile and grant admin privileges to the user
 -- database : socialnetwork, table : users
-update users set isAdmin = 1 where id = 1;
+UPDATE users SET isAdmin = 1 WHERE id = 1;
